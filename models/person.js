884293be31138ee73ef580b3c6f27ca8a7615ec2
stacy-gol <anastasiya.go@dashly.io>
2023-10-07 12:48:13 +0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -17,10 +17,23 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-  })
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minLength: 3,
+      required: [true, 'User name required']
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^(\d{2})-(\d+)/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: [true, 'User phone number required']
+    }
+  });
 
   personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
