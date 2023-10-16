539422ca62e6e290a7acc69b6ecfc32b80a5a839
stacy-gol <anastasiya.go@dashly.io>
2023-10-16 14:18:53 +0500

    4a ex.4.5 favorite blog test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLikedBlog = blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+
+  const result = {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+  return result
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
